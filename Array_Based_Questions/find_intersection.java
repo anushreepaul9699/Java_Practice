158aa1d7cac999c3617089c5d7df65470c614eec
Anushree <anushreepaul9699@gmail.com>
2023-01-09 23:17:33 +0530

    Intersection of two sorted arrays

---
+++ Array_Based_Questions/find_intersection.java
@@ -0,0 +1,57 @@
+package Array_Based_Questions;
+
+public class find_intersection 
+{
+    //defining the function -- to find the intersection (common elements)
+    /**
+     * @param arr1
+     * @param a
+     * @param arr2
+     * @param b
+     */
+    static void find_intersection_element(int[] arr1 , int a , int[] arr2 , int b)
+    {
+        //declaring two pointers
+        int pointer_1 = 0 ;
+        int pointer_2 = 0 ;
+
+        //main part --- to find the intersection
+        System.out.println("The intersection of the two array is : ") ;
+        while (pointer_1 < a && pointer_2 < b)
+        {
+            if(arr1[pointer_1] == arr2[pointer_2])
+            {
+                System.out.print(arr1[pointer_1]+" ");
+                pointer_1++ ;
+                pointer_2++ ;
+            }
+
+            else if (arr1[pointer_1] < arr2[pointer_2])
+             {
+
+                pointer_1++ ;
+            }
+
+             else
+             {
+                pointer_2++ ;
+             }
+        }
+
+    }//end of function
+    public static void main(String[] args) 
+    {
+        //declaring two sorted arrays ---- (arr1 and arr2)
+        int[] arr1 = {131 , 671 , 801 , 1212, 4567,8906} ;
+        int a = arr1.length ;
+
+        int[] arr2 = {223,801,999,1212,8906,10112} ;
+        int b = arr2.length ;
+
+        //calling the function
+        find_intersection_element(arr1 , a, arr2 , b) ;
+
+
+    }//end of main class
+    
+}//end of the program
