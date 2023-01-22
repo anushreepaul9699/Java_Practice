20247c138a230e665d620f1497b21c60b2c701b4
Anushree <anushreepaul9699@gmail.com>
2023-01-22 13:05:07 +0530

    redundant bracket present or not (useless bracket)

---
+++ Stack_Based_Questions/Redundant_or_not.java
@@ -0,0 +1,65 @@
+package Stack_Based_Questions;
+import java.util.* ;
+public class Redundant_or_not 
+{
+    public static boolean redundant_check (Stack <Character> stack , String str)
+    {
+         for(int i = 0 ; i<str.length() ; i++)
+         {
+             char ch = str.charAt(i) ;
+            
+             //agar open bracket ya koi operator milta hai to just push karenge stack me 
+             if(ch == '(' || ch =='+' || ch =='-' || ch == '*' || ch == '/')
+             {
+                 stack.push(ch) ;
+             }
+
+             //agar close bracket ya koi lower case (ignore) milta hai to check karenge ki koi operator hai ya nhi
+             // open bracket milne se phle
+
+             else
+               {
+                  if(ch == ')')
+                  {
+                      boolean isRedundant = true ;
+
+                    //jab tak koi open bracket nhi mil jata
+                    while (stack.peek() != '(')
+                    {
+                        char top = stack.peek() ;
+                         
+                        //search for operator --- > + , - , * , /
+                        if( top =='+' || top =='-' || top == '*' || top == '/')
+                        {
+                            isRedundant = false ;
+                        }
+
+                        stack.pop() ;
+                    }
+
+                    if(isRedundant == true)
+                    
+                        return true ;
+                    
+                    stack.pop() ;
+
+                  }
+
+               }
+
+               
+         }
+
+         return false ;
+    }
+    public static void main(String[] args) 
+    {
+        Stack <Character> stack = new Stack <> () ;
+        String str = "((a+b))" ;
+        boolean ans = redundant_check(stack,str);
+
+        System.out.println(ans) ;
+        
+    }//end of main class
+    
+}//end of the program
